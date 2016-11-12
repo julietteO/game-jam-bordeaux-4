cc5a5c8d79914a4247b3b055972eeffa1107e9d9
Alix <alix.fumoleau@gmail.com>
2016-11-12 17:47:07 +0100

    Add blobou reaction to the player destroying rules

---
+++ Assets/Scripts/BlobouController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine;
+
+public class BlobouController : MonoBehaviour {
+	[SerializeField] ParticleSystem joy;
+
+	void Start () {
+		ScoreManager.instance.onScoreChange += (score, total) => joy.Play(); 
+	}
+}
