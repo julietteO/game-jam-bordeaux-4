4fd9c3b0f8f047582ac0f3fc57e7462755ecec54
Alix <alix.fumoleau@gmail.com>
2016-11-12 01:07:16 +0100

    Add the teleport feature

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	Transform player;
+	SteamVR_TrackedController controller;
+	public LineRenderer line;
+
+	void Start () {
+		player = SteamVR_Render.Top().origin;
+		controller = GetComponent<SteamVR_TrackedController>();
+		controller.PadClicked += new ClickedEventHandler((_1, _2) => DisplayRay());
+		controller.PadUnclicked += new ClickedEventHandler((_1, _2) => HideRay());
+	}
+
+	void DisplayRay() {
+		line.enabled = true;
+	}
+
+	void HideRay() {
+		line.enabled = false;
+		Move();
+	}
+
+	void Move() {		
+		Plane plane = new Plane(Vector3.up, -player.position.y);
+		Ray ray = new Ray(this.transform.position, transform.forward);
+		var dist = 0f;
+		var hasGroundTarget = plane.Raycast(ray, out dist);
+		if(hasGroundTarget) {
+			Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
+			player.position = ray.origin + ray.direction * dist - new Vector3(player.GetChild(0).localPosition.x, 0f, player.GetChild(0).localPosition.z) - headPosOnGround;
+		}
+	}
+}
