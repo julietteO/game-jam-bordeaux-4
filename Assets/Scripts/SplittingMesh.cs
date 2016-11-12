f2579860c75648e592891cb331d1264b10bf6949
Alix <alix.fumoleau@gmail.com>
2016-11-12 03:16:07 +0100

    Add some tries to break cubes with the vive controller

---
+++ Assets/Scripts/SplittingMesh.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SplittingMesh : MonoBehaviour {
+
+	[SerializeField] GameObject fullForm;
+	[SerializeField] GameObject splitForm;
+	[SerializeField] float relativeVelocityThreshold;
+
+	void Start () {
+		fullForm.SetActive(true);
+		splitForm.SetActive(false);
+	}
+
+	void Split() {
+		fullForm.SetActive(false);
+		splitForm.SetActive(true);
+		splitForm.transform.SetParent(fullForm.transform.parent);
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if(collision.rigidbody == null) 
+			return;
+
+		Debug.Log(collision.transform.name+" - "+collision.rigidbody.velocity.magnitude);
+		if(collision.rigidbody.velocity.magnitude > relativeVelocityThreshold) {
+			Split();
+		}
+	}
+}
