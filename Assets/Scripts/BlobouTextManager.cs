69c474a15fe68a7de14f369a7b4308b91ba12c1a
Unknown <orain.juliette@gmail.com>
2016-11-12 09:54:57 +0100

    Init text for Blobou

---
+++ Assets/Scripts/BlobouTextManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlobouTextManager : MonoBehaviour {
+
+	int textIndex = 0;
+
+	Text textComponent;
+
+	string[] texts = new string[] {
+		"C'est bien toi, tu es le Peintre ! ",
+		"Tu peux m'aider ?"
+	};
+
+
+	// Use this for initialization
+	void Start () {
+		textComponent = GetComponent<Text> ();	
+		textComponent.text = texts [textIndex];	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void nextText() {
+		textIndex++;
+		textComponent.text = texts [textIndex];
+	}
+}
