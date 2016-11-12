de32677ff44979daf2e967219a0bba1b524599f2
Alix <alix.fumoleau@gmail.com>
2016-11-12 18:18:00 +0100

    Make blobou's face change if the player break a rule

--- Assets/Scripts/BlobouController.cs
+++ Assets/Scripts/BlobouController.cs
@@ -1,10 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class BlobouController : MonoBehaviour {
 	[SerializeField] ParticleSystem joy;
+	[SerializeField] Transform happyModel;
+	[SerializeField] Transform sadModel;
 
 	void Start () {
-		ScoreManager.instance.onScoreChange += (score, total) => joy.Play(); 
+		ScoreManager.instance.onScoreChange += (score, total) => {
+			MakeHappy();
+			if(score != total) {
+				StartCoroutine(CallAfter(MakeSad, 2.5f));
+			}
+		}; 
+	}
+
+	void MakeHappy() {
+		joy.Play();
+		happyModel.gameObject.SetActive(true);
+		sadModel.gameObject.SetActive(false);
+	}
+
+	void MakeSad() {
+		happyModel.gameObject.SetActive(false);
+		sadModel.gameObject.SetActive(true);
+	}
+
+	IEnumerator CallAfter(Action action, float delay) {
+		yield return new WaitForSeconds(delay);
+		action();
 	}
 }
