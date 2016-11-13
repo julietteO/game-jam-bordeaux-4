4894c43effc956ccb919da344d303fb2fae91796
Alix <alix.fumoleau@gmail.com>
2016-11-13 13:23:08 +0100

    Add transitions between levels

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 class ScoreManager : MonoBehaviour {
     public static ScoreManager instance;
@@ -7,6 +11,10 @@ class ScoreManager : MonoBehaviour {
     SplitMesh[] objectives;
 
     int _score;
+
+    int levelIndex = 0;
+
+    [SerializeField] Image blackScreen;
     
     public int score {
         get {
@@ -17,6 +25,9 @@ class ScoreManager : MonoBehaviour {
                 _score = value;
                 if(onScoreChange != null) {
                     onScoreChange(_score, maxScore);
+                }
+                if(_score == maxScore) {
+                    EndLevel();
                 }
             }
         }
@@ -30,11 +41,38 @@ class ScoreManager : MonoBehaviour {
 
     public event Action<int, int> onScoreChange;
 
-    void Awake() {
+    void Start() {
+        if(instance != null) {
+            Destroy(this);
+            return;
+        }
+        
         instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        StartLevel();
+    }
+
+    public void StartLevel() {
         objectives = FindObjectsOfType<SplitMesh>();
         foreach(var objective in objectives) {
             objective.onSplit += () => score++;
         }
+        score = 0;
+
+        blackScreen.color = new Color(0f, 0f, 0f, 1f);
+        blackScreen.DOFade(0f, 1f);
+    }
+
+    public void EndLevel() {
+        blackScreen.DOFade(1f, 1f).OnComplete(() => {
+            StartCoroutine(GoToNextLevel());
+        });
+    }
+
+    IEnumerator GoToNextLevel() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(++levelIndex);
+        StartLevel();
     }
 }
