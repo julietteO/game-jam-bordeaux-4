e0fce93fcd2ff58ac3836d01c7c546912abb08ea
Alix <alix.fumoleau@gmail.com>
2016-11-13 18:28:31 +0100

    Update level 2

---
+++ Assets/MaterialSwapper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MaterialSwapper : MonoBehaviour {
+	public Material swap;
+
+	public void Swap() {
+		var tmp = GetComponent<MeshRenderer>().material;
+		GetComponent<MeshRenderer>().material = swap;
+		swap = tmp;
+	}
+}
---
+++ Assets/Swap.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swap : MonoBehaviour {
+	public void DoSwap() {
+		GetComponent<Light>().enabled = !GetComponent<Light>().enabled; 
+	}
+}
